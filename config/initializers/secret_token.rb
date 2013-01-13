2867e6a8b94da22e6b1b0a24320b609010e67b02
Christopher Olsen <chrisaolsen@gmail.com>
2013-01-13 15:46:23 -0600

    added default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SustaindaneMpowerPm::Application.config.secret_token = '1e3725f501b8e9acb36d6aa32eea0a1f503ba9827d3c5e24d6259736111149db1bdf83ebd1354debd40b730850580ea7c7315d24aafe2d639e56509f0c38237c'
