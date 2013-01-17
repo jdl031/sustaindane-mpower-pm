bff8937bf2f9360370c5fe2352a4215c01f6d6ab
Christopher Olsen <chrisaolsen@gmail.com>
2013-01-16 20:56:30 -0600

    route home page through application controller, unauthenticated users are redirected to login while signed in users are sent to /companies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+  	if user_signed_in?
+  		redirect_to '/companies'
+  	else
+  		redirect_to '/users/sign_in'
+  	end
+  end
 end
