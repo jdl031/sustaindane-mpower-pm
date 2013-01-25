d695934a642a2ef3af74cb1524a658826c7f87b5
Christopher Olsen <chrisaolsen@gmail.com>
2013-01-24 20:04:46 -0600

    /tasks shows current_user's owned tasks, add My Tasks link to header

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.where('owner_id = ?', current_user.id)
 
     respond_to do |format|
       format.html # index.html.erb
