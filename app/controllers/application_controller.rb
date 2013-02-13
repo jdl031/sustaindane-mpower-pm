83bed58bfc233fca49c29bbb566a3a2b288f8408
Christopher Olsen <chrisaolsen@gmail.com>
2013-02-12 21:57:02 -0600

    when accessing an unauthorized page, redirect to root an show user friendly error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,14 @@ class ApplicationController < ActionController::Base
   def index
   	if user_signed_in?
   		if current_user.role.name == 'admin'
-  			redirect_to companies_path
+  			redirect_to companies_path, :notice => flash[:notice]
   		else
-	  		redirect_to current_user.company
+	  		redirect_to current_user.company, :notice => flash[:notice]
 	  	end
   	end
+  end
+  def permission_denied
+    flash[:notice] = "Sorry, you not allowed to access that page."
+    redirect_to root_url
   end
 end
