d65bb5253f065250489e65b3079a3145741f42fe
Christopher Olsen <chrisaolsen@gmail.com>
2013-02-25 22:29:08 -0600

    fixup ical format issues for Google Calendar

--- app/models/company.rb
+++ app/models/company.rb
@@ -8,12 +8,12 @@ class Company < ActiveRecord::Base
     puts calendar
     self.projects.each do |project|
       e=Icalendar::Event.new
-      e.uid=project.id
+      e.uid='company:'+self.id.to_s+'project:'+project.id.to_s
       e.dtstart=DateTime.civil(project.start_date.year, project.start_date.month, project.start_date.day, project.start_date.hour, project.start_date.min)
       e.dtend=DateTime.civil(project.end_date.year, project.end_date.month, project.end_date.day, project.end_date.hour, project.end_date.min)
       e.summary=project.name
-      e.created=project.created_at
-      e.last_modified=project.updated_at
+      e.created=DateTime.civil(project.created_at.year, project.created_at.month, project.created_at.day, project.created_at.hour, project.created_at.min)
+      e.last_modified=DateTime.civil(project.updated_at.year, project.updated_at.month, project.updated_at.day, project.updated_at.hour, project.updated_at.min)
       calendar.add e
     end
     calendar
