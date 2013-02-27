94a3636e767d4643a25b167c5ac26473917ba50f
Christopher Olsen <chrisaolsen@gmail.com>
2013-02-26 18:55:40 -0600

    don't increment end date, it can cause invalid dates

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,7 +18,7 @@ class Project < ActiveRecord::Base
       e=Icalendar::Event.new
       e.uid='company:'+self.company.id.to_s+':project:'+self.id.to_s+':task:'+task.id.to_s
       e.dtstart=DateTime.civil(task.due.year, task.due.month, task.due.day, task.due.hour, task.due.min)
-      e.dtend=DateTime.civil(task.due.year, task.due.month, task.due.day+1, task.due.hour, task.due.min)
+      e.dtend=DateTime.civil(task.due.year, task.due.month, task.due.day, task.due.hour, task.due.min)
       e.summary=task.title
       e.created=DateTime.civil(task.created_at.year, task.created_at.month, task.created_at.day, task.created_at.hour, task.created_at.min)
       e.last_modified=DateTime.civil(task.updated_at.year, task.updated_at.month, task.updated_at.day, task.updated_at.hour, task.updated_at.min)
